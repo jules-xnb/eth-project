b21e92ca68f63d4c8c55e9d960c562de3ee5bf12
Luis <pereiravazluis@gmail.com>
2021-01-25 19:41:32 +0100

    Prepare ready to migrate 2

--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -1,17 +1,12 @@
 import Web3 from "web3";
-import ComplexStorage from "./contracts/ComplexStorage.json";
-import SimpleStorage from "./contracts/SimpleStorage.json";
-import TutorialToken from "./contracts/TutorialToken.json";
+import Casino from "./contracts/Casino.json";
 
 const options = {
   web3: {
     block: false,
     customProvider: new Web3("ws://localhost:7545"),
   },
-  contracts: [SimpleStorage, ComplexStorage, TutorialToken],
-  events: {
-    SimpleStorage: ["StorageSet"],
-  },
+  contracts: [Casino],
 };
 
 export default options;
