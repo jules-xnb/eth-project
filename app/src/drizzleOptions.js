b557c6b923448d4d8626fbbfe54bbfbc5a742db8
Luis <pereiravazluis@gmail.com>
2021-01-25 20:02:29 +0100

    Prepare to migrate contracts

--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -1,12 +1,15 @@
 import Web3 from "web3";
 import Casino from "./contracts/Casino.json";
+import Ownable from "./contracts/Ownable.json";
+import Token from "./contracts/Token.json";
+import UserContract from "./contracts/UserContract.json";
 
 const options = {
   web3: {
     block: false,
     customProvider: new Web3("ws://localhost:7545"),
   },
-  contracts: [Casino],
+  contracts: [Casino, Ownable, Token, UserContract],
 };
 
 export default options;
