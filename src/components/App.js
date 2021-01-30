4cd992fe0c7cf1cea9c83634a7b02103c43986c8
Luis <pereiravazluis@gmail.com>
2021-01-30 22:07:06 +0100

    Front no bug (avant mise en place fonction front)

--- src/components/App.js
+++ src/components/App.js
@@ -24,37 +24,37 @@ class App extends Component {
     const networkId = await web3.eth.net.getId()
 
     // Load DaiToken
-    const daiTokenData = DaiToken.networks[networkId]
-    if(daiTokenData) {
-      const daiToken = new web3.eth.Contract(DaiToken.abi, daiTokenData.address)
-      this.setState({ daiToken })
-      let daiTokenBalance = await daiToken.methods.balanceOf(this.state.account).call()
-      this.setState({ daiTokenBalance: daiTokenBalance.toString() })
-    } else {
-      window.alert('DaiToken contract not deployed to detected network.')
-    }
+    // const daiTokenData = DaiToken.networks[networkId]
+    // if(daiTokenData) {
+    //   const daiToken = new web3.eth.Contract(DaiToken.abi, daiTokenData.address)
+    //   this.setState({ daiToken })
+    //   let daiTokenBalance = await daiToken.methods.balanceOf(this.state.account).call()
+    //   this.setState({ daiTokenBalance: daiTokenBalance.toString() })
+    // } else {
+    //   window.alert('DaiToken contract not deployed to detected network.')
+    // }
 
     // Load DappToken
-    const dappTokenData = DappToken.networks[networkId]
-    if(dappTokenData) {
-      const dappToken = new web3.eth.Contract(DappToken.abi, dappTokenData.address)
-      this.setState({ dappToken })
-      let dappTokenBalance = await dappToken.methods.balanceOf(this.state.account).call()
-      this.setState({ dappTokenBalance: dappTokenBalance.toString() })
-    } else {
-      window.alert('DappToken contract not deployed to detected network.')
-    }
+    // const dappTokenData = DappToken.networks[networkId]
+    // if(dappTokenData) {
+    //   const dappToken = new web3.eth.Contract(DappToken.abi, dappTokenData.address)
+    //   this.setState({ dappToken })
+    //   let dappTokenBalance = await dappToken.methods.balanceOf(this.state.account).call()
+    //   this.setState({ dappTokenBalance: dappTokenBalance.toString() })
+    // } else {
+    //   window.alert('DappToken contract not deployed to detected network.')
+    // }
 
     // Load TokenFarm
-    const tokenFarmData = TokenFarm.networks[networkId]
-    if(tokenFarmData) {
-      const tokenFarm = new web3.eth.Contract(TokenFarm.abi, tokenFarmData.address)
-      this.setState({ tokenFarm })
-      let stakingBalance = await tokenFarm.methods.stakingBalance(this.state.account).call()
-      this.setState({ stakingBalance: stakingBalance.toString() })
-    } else {
-      window.alert('TokenFarm contract not deployed to detected network.')
-    }
+    // const tokenFarmData = TokenFarm.networks[networkId]
+    // if(tokenFarmData) {
+    //   const tokenFarm = new web3.eth.Contract(TokenFarm.abi, tokenFarmData.address)
+    //   this.setState({ tokenFarm })
+    //   let stakingBalance = await tokenFarm.methods.stakingBalance(this.state.account).call()
+    //   this.setState({ stakingBalance: stakingBalance.toString() })
+    // } else {
+    //   window.alert('TokenFarm contract not deployed to detected network.')
+    // }
 
     this.setState({ loading: false })
   }
@@ -72,32 +72,32 @@ class App extends Component {
     }
   }
 
-  stakeTokens = (amount) => {
-    this.setState({ loading: true })
-    this.state.daiToken.methods.approve(this.state.tokenFarm._address, amount).send({ from: this.state.account }).on('transactionHash', (hash) => {
-      this.state.tokenFarm.methods.stakeTokens(amount).send({ from: this.state.account }).on('transactionHash', (hash) => {
-        this.setState({ loading: false })
-      })
-    })
-  }
+  // stakeTokens = (amount) => {
+  //   this.setState({ loading: true })
+  //   this.state.daiToken.methods.approve(this.state.tokenFarm._address, amount).send({ from: this.state.account }).on('transactionHash', (hash) => {
+  //     this.state.tokenFarm.methods.stakeTokens(amount).send({ from: this.state.account }).on('transactionHash', (hash) => {
+  //       this.setState({ loading: false })
+  //     })
+  //   })
+  // }
 
-  unstakeTokens = (amount) => {
-    this.setState({ loading: true })
-    this.state.tokenFarm.methods.unstakeTokens().send({ from: this.state.account }).on('transactionHash', (hash) => {
-      this.setState({ loading: false })
-    })
-  }
+  // unstakeTokens = (amount) => {
+  //   this.setState({ loading: true })
+  //   this.state.tokenFarm.methods.unstakeTokens().send({ from: this.state.account }).on('transactionHash', (hash) => {
+  //     this.setState({ loading: false })
+  //   })
+  // }
 
   constructor(props) {
     super(props)
     this.state = {
       account: '0x0',
-      daiToken: {},
-      dappToken: {},
-      tokenFarm: {},
-      daiTokenBalance: '0',
-      dappTokenBalance: '0',
-      stakingBalance: '0',
+      // daiToken: {},
+      // dappToken: {},
+      // tokenFarm: {},
+      // daiTokenBalance: '0',
+      // dappTokenBalance: '0',
+      // stakingBalance: '0',
       loading: true
     }
   }
